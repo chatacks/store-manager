484a44c4f3a3e363abf6d4c8dde8a6824e025212
Thyago <chatack.tc@gmail.com>
2024-02-25 13:56:42 -0300

    :test_tube: [test]: implementa funções de testes unitários para sales:Services

---
+++ backend/tests/unit/services/sales.services.test.js
@@ -0,0 +1,45 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const { salesModel } = require('../../../src/models');
+const { allSalesMockModel, saleByIdMockModel } = require('../mocks/sales.mock');
+const { salesService } = require('../../../src/services');
+
+describe('Testes unitários Sales:Services', function () {
+  afterEach(function () {
+    sinon.restore();
+  });
+
+  it('Recupera com SUCCESSFUL todas as vendas pela camada Service', async function () {
+    sinon.stub(salesModel, 'getAllSales').resolves(allSalesMockModel);
+
+    const response = await salesService.getAllSalesService();
+
+    expect(response.status).to.be.equal('SUCCESSFUL');
+    expect(response.data).to.be.an('array');
+    expect(response.data).to.be.deep.equal(allSalesMockModel);
+  });
+
+  it('Recupera com SUCCESFUL a venda correspondente ao id recebido', async function () {
+    sinon.stub(salesModel, 'getSaleById').resolves(saleByIdMockModel);
+    const saleId = 1;
+
+    const response = await salesService.getSaleByIdService(saleId);
+
+    expect(response.status).to.be.equal('SUCCESSFUL');
+    expect(response.data).to.be.an('object');
+    expect(response.data).to.be.deep.equal(saleByIdMockModel);
+  });
+
+  it('Ao receber um id inválido retorna um error', async function () {
+    sinon.stub(salesModel, 'getSaleById').resolves(undefined);
+    const saleId = 999;
+
+    const response = await salesService.getSaleByIdService(saleId);
+
+    expect(response.status).to.be.equal('NOT_FOUND');
+    expect(response.data).to.be.an('object');
+    expect(response.data).to.be.deep.equal({
+      message: 'Sale not found',
+    });
+  });
+});
