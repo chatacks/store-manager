e083805a0b2046ee2c6179e95491904a2bd3edea
Thyago <chatack.tc@gmail.com>
2024-02-25 12:09:24 -0300

    :test_tube: [test]: implementa funções de testes unitários para sales:Model e mocks para a mesma

---
+++ backend/tests/unit/mocks/sales.mock.js
@@ -0,0 +1,31 @@
+const allSalesMockModel = [
+  {
+    saleId: 1,
+    date: '2024-02-24T22:20:22.000Z',
+    productId: 1,
+    quantity: 5,
+  },
+  {
+    saleId: 1,
+    date: '2024-02-24T22:20:22.000Z',
+    productId: 2,
+    quantity: 10,
+  },
+  {
+    saleId: 2,
+    date: '2024-02-24T22:20:22.000Z',
+    productId: 3,
+    quantity: 15,
+  },
+];
+
+const saleByIdMockModel = {
+  date: '2024-02-24T22:20:22.000Z',
+  productId: 1,
+  quantity: 5,
+};
+
+module.exports = {
+  allSalesMockModel,
+  saleByIdMockModel,
+};
---
+++ backend/tests/unit/models/sales.model.test.js
@@ -0,0 +1,30 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+const connection = require('../../../src/models/connection');
+const { allSalesMockModel, saleByIdMockModel } = require('../mocks/sales.mock');
+const { salesModel } = require('../../../src/models');
+
+describe('Testes Unitários Sales:Model', function () {
+  afterEach(function () {
+    sinon.restore();
+  });
+
+  it('Recupera todas as vendas com sucesso', async function () {
+    sinon.stub(connection, 'execute').resolves([allSalesMockModel]);
+
+    const response = await salesModel.getAllSales();
+
+    expect(response).to.be.an('array');
+    expect(response).to.be.deep.equal(allSalesMockModel);
+  });
+
+  it('Recupera com sucesso a venda correspondente ao id correto', async function () {
+    sinon.stub(connection, 'execute').resolves([[saleByIdMockModel]]);
+    const saleId = 1;
+    
+    const response = await salesModel.getSaleById(saleId);
+
+    expect(response).to.be.an('object');
+    expect(response).to.be.deep.equal(saleByIdMockModel);
+  });
+});
