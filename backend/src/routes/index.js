5b8ec3618f229839c55b95cdacafbe630b403e9f
Thyago <chatack.tc@gmail.com>
2024-02-25 11:37:34 -0300

    [feat]: cria barrel para sales em controller e routes

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,6 +1,8 @@
 const appRouter = require('express').Router();
 const productsRouter = require('./products.router');
+const salesRouter = require('./sales.router');
 
 appRouter.use('/products', productsRouter);
+appRouter.use('/sales', salesRouter);
 
 module.exports = appRouter;
